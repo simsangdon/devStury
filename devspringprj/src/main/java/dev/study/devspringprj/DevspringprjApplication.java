027d3a252cf6b907a616f97abf74962936077876
simsangdon <hahrdhaly12@gmail.com>
2022-04-09 12:02:19 +0900

    초기 설정 테스트

---
+++ devspringprj/src/main/java/dev/study/devspringprj/DevspringprjApplication.java
@@ -0,0 +1,13 @@
+package dev.study.devspringprj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevspringprjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevspringprjApplication.class, args);
+	}
+
+}
---
+++ devspringprj/src/test/java/dev/study/devspringprj/DevspringprjApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.study.devspringprj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevspringprjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
