935e41810e77bd42849c10ca9c37288726ffaa38
simsangdon <hahrdhaly12@gmail.com>
2022-04-09 12:08:09 +0900

    코딩 테스트

---
+++ devspringprj/src/main/java/dev/study/devspringprj/HelloController.java
@@ -0,0 +1,20 @@
+package dev.study.devspringprj;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+
+@Controller
+public class HelloController {
+
+    @RequestMapping("/")
+    public String index(@RequestParam(name = "name" , required = false, defaultValue = "World"  )String name
+                      , @RequestParam(name = "value", required = false, defaultValue = "Spring" )String value
+                      , Model model){
+        model.addAttribute("name" , name );
+        model.addAttribute("value", value);
+    
+        return "index";
+    }
+}
---
+++ devspringprj/src/main/webapp/WEB-INF/template/index.jsp
@@ -0,0 +1,13 @@
+<%@ page language="java" contentType="text/html; charset=utf-8" pageEncoding="utf-8"%>
+<!DOCTYPE  html>
+<html>
+    <head>
+        <title>Hello, ${name}, ${value}</title>
+    </head>
+    <script>
+        alert("테스트 ${name}, ${value}");
+    </script>
+    <body>
+        <div>Hello, ${name}, ${value}심상돈</div>
+    </body>
+</html>
