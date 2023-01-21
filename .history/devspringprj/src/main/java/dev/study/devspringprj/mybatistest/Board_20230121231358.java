40bb0d2e3785f63b326a65d04e38eb2932ff1ac9
donbq <hahrdhaly12@gmail.com>
2023-01-22 00:32:55 +0900

    수정

--- .history/devspringprj/src/main/java/dev/study/devspringprj/mybatistest/Board_20230121231358.java
+++ .history/devspringprj/src/main/java/dev/study/devspringprj/mybatistest/Board_20230121231358.java
@@ -16,64 +16,49 @@ public class Board {
     public String getIdx() {
         return idx;
     }
-
     public void setIdx(String idx) {
         this.idx = idx;
     }
-
     public String getParentIdx() {
         return parentIdx;
     }
-
     public void setParentIdx(String parentIdx) {
         this.parentIdx = parentIdx;
     }
-
     public String getTitle() {
         return title;
     }
-
     public void setTitle(String title) {
         this.title = title;
     }
-
     public String getContents() {
         return contents;
     }
-
     public void setContents(String contents) {
         this.contents = contents;
     }
-
     public String getHitCnt() {
         return hitCnt;
     }
-
     public void setHitCnt(String hitCnt) {
         this.hitCnt = hitCnt;
     }
-
     public String getDelGb() {
         return delGb;
     }
-
     public void setDelGb(String delGb) {
         this.delGb = delGb;
     }
-
     public String getCreaDtm() {
         return creaDtm;
     }
-
     public void setCreaDtm(String creaDtm) {
         this.creaDtm = creaDtm;
     }
-
     public String getCreaId() {
         return creaId;
     }
-
     public void setCreaId(String creaId) {
         this.creaId = creaId;
-    }
+    }    
 }
